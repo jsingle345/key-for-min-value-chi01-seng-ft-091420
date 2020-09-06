8a24697648a5b79eba321afdafd0dcb441265c26
Joshua Singleton <jsingle345@gmail.com>
2020-09-06 20:46:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,8 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-name_hash = {:chair => 25, :table => 85, :mattress => 450}
- i = 0
+
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|.min
     puts key
