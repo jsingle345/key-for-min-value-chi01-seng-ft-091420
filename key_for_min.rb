9fe63688ca50aed09ba663c2301efcffb6f1d9c5
Joshua Singleton <jsingle345@gmail.com>
2020-09-06 20:26:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 name_hash = {:chair => 25, :table => 85, :mattress => 450}
+ i = 0
 def key_for_min_value(name_hash)
  while  name_hash
  if 
