fa5007aea980f11c34d200eb4111774193ca0c28
Joshua Singleton <jsingle345@gmail.com>
2020-09-06 21:01:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,22 @@
 
 
 def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
   name_hash.each do |key, value|
+   if value < lowest_value 
+     
+end
+
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
   end
-  name_hash
+  lowest_key
 end
